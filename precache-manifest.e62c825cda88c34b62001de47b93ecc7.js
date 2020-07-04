07c54be807e5ebe016fa9c37b20afae711bfa739
lana wong <lana_1123@hotmail.com>
2020-07-05 02:05:52 +0800

    Updates

---
+++ precache-manifest.e62c825cda88c34b62001de47b93ecc7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817d42d1f346ef969d9e79f8b604d4a2",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "ac72ab8591a00c3e0b52",
+    "url": "/weather/static/css/2.23e0c18e.chunk.css"
+  },
+  {
+    "revision": "b36b399ba0cd9bc61c95",
+    "url": "/weather/static/css/main.c70152ee.chunk.css"
+  },
+  {
+    "revision": "ac72ab8591a00c3e0b52",
+    "url": "/weather/static/js/2.a65f96e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.a65f96e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b36b399ba0cd9bc61c95",
+    "url": "/weather/static/js/main.e4baaa6f.chunk.js"
+  },
+  {
+    "revision": "cbd170559497437f72ba",
+    "url": "/weather/static/js/runtime-main.f775bfa1.js"
+  },
+  {
+    "revision": "e26cc5037010768e18178df8ad7cb48b",
+    "url": "/weather/static/media/White-Heaven-Background.e26cc503.png"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weather/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
